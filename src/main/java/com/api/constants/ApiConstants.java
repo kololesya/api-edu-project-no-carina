141a2dd34b970dd42ba3d58b05e6c00d95f0b19d
Olesya Kolenchenko <kololesya@gmail.com>
2025-05-13 13:11:35 +0200

    Clean up unwanted tracked files using .gitignore

--- src/main/java/com/api/constants/ApiConstants.java
+++ src/main/java/com/api/constants/ApiConstants.java
@@ -1,2 +1,9 @@
-package com.api.constants;public class ApiConstants {
+package com.api.constants;
+
+import com.api.config.ConfigLoader;
+
+public class ApiConstants {
+    public static final String BASE_URL = ConfigLoader.get("base.url");
+    public static final String BEARER_TOKEN = ConfigLoader.get("bearer.token");
 }
+
