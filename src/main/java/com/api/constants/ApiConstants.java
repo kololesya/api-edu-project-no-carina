cddf1b300aba86b134b3b8cded2ecb26c4ab7a90
Olesya Kolenchenko <kololesya@gmail.com>
2025-05-14 14:48:29 +0200

    Reset main to contain only initial files

--- src/main/java/com/api/constants/ApiConstants.java
+++
@@ -1,9 +0,0 @@
-package com.api.constants;
-
-import com.api.config.ConfigLoader;
-
-public class ApiConstants {
-    public static final String BASE_URL = ConfigLoader.get("base.url");
-    public static final String BEARER_TOKEN = ConfigLoader.get("bearer.token");
-}
-
--- src/main/java/com/api/constants/HttpMethod.java
+++
@@ -1,5 +0,0 @@
-package com.api.constants;
-
-public enum HttpMethod {
-    GET, POST, PUT, DELETE
-}
