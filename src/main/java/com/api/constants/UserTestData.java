ed6415518cc7f0ee57240b6e8dc34e44f7bc9d33
Olesya Kolenchenko <kololesya@gmail.com>
2025-06-02 11:33:33 +0200

    refactoring updateUserTest after resolving conflict

--- src/main/java/com/api/constants/UserTestData.java
+++ src/main/java/com/api/constants/UserTestData.java
@@ -7,8 +7,8 @@ public class UserTestData {
     public static final String VALID_NAME = TestDataConfig.get("user.name");
     public static final String VALID_GENDER = TestDataConfig.get("user.gender");
     public static final String VALID_STATUS = TestDataConfig.get("user.status");
-    public static final String UPDATED_NAME = TestDataConfig.get("user.updateName");
-    public static final String UPDATED_STATUS = TestDataConfig.get("user.updateStatus");
+    public static final String UPDATED_NAME = TestDataConfig.get("user.updated.name");
+    public static final String UPDATED_STATUS = TestDataConfig.get("user.updated.status");
 
     public static final String INVALID_EMAIL = "invalid-email-format";
     public static final String INVALID_GENDER = "alien";
--- src/test/java/com/api/GraphQLTests.java
+++ src/test/java/com/api/GraphQLTests.java
@@ -9,6 +9,8 @@ import org.testng.annotations.Test;
 import com.api.models.UserFactory;
 
 import static com.api.constants.HttpStatus.OK;
+import static com.api.models.UserFactory.createDefaultUserDataWithRandomEmail;
+import static com.api.models.UserFactory.createUpdatedUserData;
 import static com.api.utils.GraphQLResponseValidator.*;
 
 public class GraphQLTests extends BaseGraphQLTest {
@@ -21,7 +23,7 @@ public class GraphQLTests extends BaseGraphQLTest {
 
     @Test
     public void getUserByIdTest() {
-        Map<String, String> expectedData = UserFactory.createDefaultUserData();
+        Map<String, String> expectedData = createDefaultUserDataWithRandomEmail();
         int userId = graphQLService.createUserAndGetId(expectedData);
         Map<String, String> actualData = graphQLService.getUserDataById(userId);
         validateUserData(expectedData, actualData);
@@ -29,7 +31,7 @@ public class GraphQLTests extends BaseGraphQLTest {
 
     @Test
     public void createUserTest() {
-        Map<String, String> expectedData = UserFactory.createDefaultUserData();
+        Map<String, String> expectedData = createDefaultUserDataWithRandomEmail();
         int userId = graphQLService.createUserAndGetId(expectedData);
         Map<String, String> actualData = graphQLService.getUserDataById(userId);
         validateUserData(expectedData, actualData);
@@ -37,9 +39,9 @@ public class GraphQLTests extends BaseGraphQLTest {
 
     @Test
     public void updateUserTest() {
-        Map<String, String> originalData = UserFactory.createDefaultUserData();
+        Map<String, String> originalData = createDefaultUserDataWithRandomEmail();
         int userId = graphQLService.createUserAndGetId(originalData);
-        Map<String, String> updatedData = UserFactory.updateUserData();
+        Map<String, String> updatedData = createUpdatedUserData(originalData);
         HttpResponse<String> response = graphQLService.updateUser(userId, updatedData);
         Assert.assertEquals(response.statusCode(), OK.getCode(), "Expected status code 200 after user update");
         Assert.assertFalse(response.body().contains("errors"), "Response contains GraphQL errors");
@@ -49,7 +51,7 @@ public class GraphQLTests extends BaseGraphQLTest {
 
     @Test
     public void deleteUserTest() {
-        Map<String, String> originalData = UserFactory.createDefaultUserData();
+        Map<String, String> originalData = createDefaultUserDataWithRandomEmail();
         int userId = graphQLService.createUserAndGetId(originalData);
         HttpResponse<String> deleteResponse = graphQLService.deleteUser(userId);
         Assert.assertEquals(deleteResponse.statusCode(), OK.getCode(), "Expected status 200 after deletion");
